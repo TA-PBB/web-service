ff3393326e607dea52f618d1c0c1df184058988b
syasul <githubku4@gmail.com>
2024-06-20 19:04:27 +0700

    p

--- TaskMasterService/wsgi.py
+++ TaskMasterService/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for TaskMaster project.
+WSGI config for TaskMasterService project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TaskMaster.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TaskMasterService.settings')
 
 application = get_wsgi_application()
